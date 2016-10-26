52da2ff6f7d0e24c651b50a6bd0a51b383dab54e
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-25 17:58:49 -0700

    Left in the middle of the signup function in server.py

--- server.py
+++ server.py
@@ -2,10 +2,11 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask, jsonify
+from flask import (Flask, render_template, redirect, request, flash,
+                   session, jsonify)
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -22,8 +23,37 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    a = jsonify([1,3])
-    return a
+    # a = jsonify([1,3])
+
+    return render_template("homepage.html")
+
+@app.route('/signup_catch', methods=['POST'])
+    """Collect results from signup form"""
+    # Setup rules for usernames
+    # Setup rules for password
+    #Constrain age to numbers less than 200
+    #Constrain zipcode to [0-9]{5}
+    username = request.form,get("username")
+    if username != 
+    password = request.form,get("password")
+    age = request.form,get("age")
+    zipcode = request.form,get("zipcode")
+    return redirect('/')
+
+@app.route('/login_catch', methods=['POST'])
+    """Collect results from login form"""
+    #check database for user id #On failure: FLASH 
+    #Check matching password #On failure FLASH
+    return redirect('/')
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+
 
 
 if __name__ == "__main__":
