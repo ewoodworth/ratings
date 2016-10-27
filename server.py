7f9d9317716280b6f2b1d403dec7ca0fe25c71d7
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-26 18:01:35 -0700

    Signup complete. Login function in progress

--- server.py
+++ server.py
@@ -18,7 +18,8 @@ app.secret_key = "ABC"
 # silently. This is horrible. Fix this so that, instead, it raises an
 # error.
 app.jinja_env.undefined = StrictUndefined
-
+#Fix server-side caching issues
+app.jinja_env.auto_reload = True
 
 @app.route('/')
 def index():
@@ -26,8 +27,14 @@ def index():
     # a = jsonify([1,3])
     #SETUP SESSION W?USER KEY NULL START< FILL UPON ESTABLISHMENT
     # current_user = session.get(current_user:{username:, }, 0)
-    
-    return render_template("homepage.html")
+    if 'current_user' in session:
+        #How to render a page if they are already signed inde
+        return render_template('homepage.html')
+    else:
+        # current_user = session['current_user'] = {} 
+        #how to render a page for login/signin
+        return render_template('signup.html')
+
 
 # @app.route('/dbcheck', methods=['POST'])
 # def dbcheck():
@@ -49,10 +56,11 @@ def signup_catch():
         password = request.form.get("password")
         age = request.form.get("age")
         zipcode = request.form.get("zipcode")
-        user = User(user_id=user_id, username=username, password=password, age=age, zipcode=zipcode)
+        user = User(username=username, password=password, age=age, zipcode=zipcode)
         db.session.add(user)
         db.session.commit()
-
+        #create session cookie for this browser session
+        session['current_user'] = {'username':username}
 
 
     return redirect('/')
@@ -62,6 +70,15 @@ def signup_catch():
     # """Collect results from login form"""
     #check database for user id #On failure: FLASH 
     #Check matching password #On failure FLASH
+    username = request.form.get("username")
+    password = request.form.get("password")
+    check_username = User.query.filter_by(username=username).first()
+    check_password = User.query.filter_by(password=password).first()
+    if check_username AND check_password:
+        return render_template('homepage.html')
+    else:
+        flash('Your username and password were not recognized')
+        return render_template('signup.html')
     # return redirect('/')
 
 @app.route("/users")
--- static/script.js
+++ static/script.js
@@ -1,5 +1,17 @@
-function checkUsername(evt){
-    evt.preventDefault();
-    $("#userid")
-}
-$("#username").on("submit", checkUsername);
+// function dbCheck(result){
+//     var inDb = result;
+//     if (inDb) {
+//         $('#dbCheck').html('This username is already taken!');
+//     }
+// }
+
+
+// function checkUsername(evt){
+//     evt.preventDefault();
+//     var usernameVal = $('#username').val();
+//     $.post('/dbcheck', usernameVal, dbCheck);
+//     if () {
+//         continue
+//     }
+// }
+// $("#username").on("submit", checkUsername);
